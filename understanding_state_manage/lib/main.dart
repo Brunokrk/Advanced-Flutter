e738e1268e6f89a3ac1f188539107d9a704b657a
Bruno Marchi Pires <45456467+Brunokrk@users.noreply.github.com>
2025-04-10 21:25:26 -0300

    Projeto Vazio para iniciar novas implementações

--- understanding_state_manage/lib/main.dart
+++ understanding_state_manage/lib/main.dart
@@ -1,30 +1,30 @@
 import 'package:flutter/material.dart';
-import 'package:understanding_state_manage/presentation/controllers/theme_controller.dart';
-
-import 'presentation/pages/my_home_page.dart';
-
 // ctrl + K  -> ctrl + S
-///Trocar Tema da aplicação entre claro e escuro utilizando valueNotifier
-///
+
 void main() {
   runApp(const MyApp());
 }
-
-final themeController = ThemeController();
-
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return ValueListenableBuilder(
-      valueListenable: themeController,
-      builder: (context, value, child) {
-        return MaterialApp(
-          title: 'Flutter Demo',
-          theme: value ? ThemeData.dark(): ThemeData.light(),
-          home: MyHomePage(),
-        );
-      }
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(primarySwatch: Colors.purple),
+      home: MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Gerenciamento de estado"),
+      ),
     );
   }
 }
