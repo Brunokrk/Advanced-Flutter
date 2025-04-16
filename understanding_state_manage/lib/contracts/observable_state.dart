9df123d53c46900373a5e234b35df836655669af
Bruno Marchi Pires <45456467+Brunokrk@users.noreply.github.com>
2025-04-16 19:33:02 -0300

    gerenciamento de estado unico criado

--- understanding_state_manage/lib/contracts/observable_state.dart
+++ understanding_state_manage/lib/contracts/observable_state.dart
@@ -1,4 +1,4 @@
 abstract class ObservableState<T>{
-  final T state; //value
+  final T state; // similar ao value
   ObservableState(this.state);
 }
--- understanding_state_manage/lib/controllers/state_observable.dart
+++ understanding_state_manage/lib/controllers/state_observable.dart
@@ -2,15 +2,15 @@ import 'package:understanding_state_manage/contracts/observable_state.dart';
 import 'package:understanding_state_manage/controllers/change_state.dart';
 
 class StateObservable<T> extends ChangeState implements ObservableState{
+  T _state;
 
-
-  final T _state;
-
-  @override
+  @override 
   T get state => _state;
 
-  set state( T newState){
-
+  set state(T newState){
+    if(newState == _state) return;//aplicação possui um estado, e o novo emitido é igual ao anterior -> nada feito
+    _state = newState;
+    notifyCallbacks();
   }
 
   StateObservable(this._state);
