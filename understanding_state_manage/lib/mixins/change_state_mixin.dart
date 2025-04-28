0d91e2c23261a1d5b88574dfc078bdfdc1e1b70d
Bruno Marchi Pires <45456467+Brunokrk@users.noreply.github.com>
2025-04-28 20:38:49 -0300

    .

--- understanding_state_manage/lib/mixins/change_state_mixin.dart
+++ understanding_state_manage/lib/mixins/change_state_mixin.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:understanding_state_manage/controllers/change_state.dart';
+import 'package:understanding_state_manage/controllers/state_observable.dart';
 
 mixin ChangeStateMixin<T extends StatefulWidget> on State<T> {
 
@@ -10,6 +11,13 @@ mixin ChangeStateMixin<T extends StatefulWidget> on State<T> {
   void useChangeState(ChangeState changeState){
     changeState.addListener(_callback);
     _changeStates.add(changeState);
+  }
+
+  StateObservable<T> useStateObservable<T>(T state) {
+    final stateObservable =  StateObservable<T>(state);
+    stateObservable.addListener(_callback);
+    _changeStates.add(stateObservable);
+    return stateObservable;
   }
 
   void _callback(){
---
+++ understanding_state_manage/test/change_state_test.dart
@@ -0,0 +1,43 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:understanding_state_manage/controllers/change_state.dart';
+
+
+void main(){
+
+  //Padrão de testes AAA
+  group("Should test ChangeState", (){
+    test("Should increment counter", (){
+      //Arrange
+      final _CounterChangeState changeState = _CounterChangeState();
+
+      //Act
+      changeState.increment();
+
+      //Assert
+      expect(changeState.counter, 1);
+    });
+
+     test("Should increment counter", (){
+      //Arrange
+      final _CounterChangeState changeState = _CounterChangeState();
+
+      //Act
+      changeState.increment();
+
+      //Assert
+      expect(changeState.counter, 1);
+    });
+
+  });
+
+}
+
+class _CounterChangeState extends ChangeState{
+  int counter = 0;
+
+  void increment(){
+    counter ++;
+    notifyCallbacks();
+  }
+
+}
